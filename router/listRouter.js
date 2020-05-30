69e51d505cd74e455cc6b70cfcd5af9fd8b2833a
chenMM <1197729488@qq.com>
2020-05-30 11:26:12 +0800

    update

--- router/listRouter.js
+++ router/listRouter.js
@@ -38,7 +38,7 @@ listRouter.get('/rpop', (req, res) => {
 
 listRouter.get('/lpop', (req, res) => {
   //使用list  ->lpop
-  client.rpop('stars', (error, result) => {
+  client.lpop('stars', (error, result) => {
     if (error) {
       res.send({code: -1, msg: 'request error', error})
     } else {
@@ -47,15 +47,5 @@ listRouter.get('/lpop', (req, res) => {
   })
 })
 
-listRouter.get('/all', (req, res) => {
-  //获取对象
-  client.hgetall('user', (error, result) => {
-    if (error) {
-      res.send({code: -1, msg: 'request error', error})
-    } else {
-      res.send({code: 0, msg: 'request success', result})
-    }
-  })
-})
 
 module.exports = listRouter
